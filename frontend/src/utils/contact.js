bd4998a542bfe434addcf7c948e6c8b9f3da486d
Shreyansh Shukla <shreyansh.14010@gmail.com>
2026-04-25 19:29:31 +0530

    Minor changes

--- frontend/src/utils/contact.js
+++ frontend/src/utils/contact.js
@@ -1,20 +1,20 @@
 export const contactData = [
-    {
-      title: "Truck Related Queries",
-      description: "GPS, FASTag & more",
-      phone: "9266339683",
-      email: "enterprisesolutions@FarmXpress.com",
-    },
-    {
-      title: "Load Related Queries",
-      description: "Get instant quotes",
-      phone: "9370093700",
-      email: "support@FarmXpress.com",
-    },
-    {
-      title: "Call Our Helpline",
-      description: "24x7 customer support",
-      phone: "9370093700",
-      email: "care@FarmXpress.com",
-    },
-  ];
+  {
+    title: "Truck Related Queries",
+    description: "GPS, FASTag & more",
+    phone: "9989856452",
+    email: "faq@FarmXpress.com",
+  },
+  {
+    title: "Load Related Queries",
+    description: "Get instant quotes",
+    phone: "9989856452",
+    email: "support@FarmXpress.com",
+  },
+  {
+    title: "Call Our Helpline",
+    description: "24x7 customer support",
+    phone: "9989856452",
+    email: "care@FarmXpress.com",
+  },
+];
